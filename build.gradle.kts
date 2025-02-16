95f52aa45c1befbd415a402c636f3b4eafbff526
Fabian Zeindl <fabian.zeindl@gmail.com>
2025-02-16 06:55:34 +0100

    [code] upgrade to gradle 8.12.1, kotlin 2.1.10, kotlin-json-stream-serializer-1.2.3, kotlin-json-stream-2.0.3, ksp 2.1.10-1.0.30

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm").version("2.1.10")
+    id("com.google.devtools.ksp") version("2.1.10-1.0.30")
+}
+
+group = "com.fab1an"
+version = "2.0.3-SNAPSHOT"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    ksp("com.fab1an:kotlin-json-stream-serializer:2.0.3")
+    implementation("com.fab1an:kotlin-json-stream-serializer-annotations:2.0.0")
+    implementation("com.fab1an:kotlin-json-stream:1.2.3")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
